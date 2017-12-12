03ad8812f6b78df92a0338caf47be3da3e9d202b
Thodoris <tpapak@gmail.com>
2017-12-12 15:34:15 +0100

    githubsources

--- R/wide2long.R
+++ R/wide2long.R
@@ -1,14 +1,13 @@
 
 wide2Long = function (data,type="binary") {
   library(V8)
-  library(readxl)
   js <- V8::v8(typed_arrays = TRUE)
 #js$source(file=system.file("js/underscore-min.js", package="V8"))
-  js$source(file = "js/underscore-min.js")
+  js$source("https://raw.githubusercontent.com/esm-ispm-unibe-ch/dataformatter/master/js/underscore-min.js")
   #js$source(file="js/combinations.js")
-  js$source(file="js/wide2Long.js")
-  lkj = js$call("wideToLong",data,type)
-  return (lkj)
+  js$source("https://raw.githubusercontent.com/esm-ispm-unibe-ch/dataformatter/master/js/wide2Long.js")
+  result = js$call("wideToLong",data,type)
+  return (result)
 }
 
 
