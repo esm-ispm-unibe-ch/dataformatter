cf056ae9e303e02de95fe04bc4a8015febce9d3c
Thodoris <tpapak@gmail.com>
2017-12-12 15:28:12 +0100

    depedencies

--- test/test.R
+++ test/test.R
@@ -1,7 +1,7 @@
 Sys.setenv(LANG = "en")
 #rm(list=ls())
-wbin <- read_excel("~/Documents/data-formatter/acutemania_netwide_multiarm_binary.xls")
-wcont <- read_excel("~/Documents/data-formatter/CPPS_netwide_multiarm_continuous.xls")
+wbin <- read_excel("./acutemania_netwide_multiarm_binary.xls")
+wcont <- read_excel("./CPPS_netwide_multiarm_continuous.xls")
 
 outbin = wide2Long(wbin,"binary")
 print(outbin)
