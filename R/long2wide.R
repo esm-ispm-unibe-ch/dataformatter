6e977daa1b03eca07b286ef3a07d6a8c91c73a17
Thodoris Papakonstantinou <mail@tpapak.com>
2018-10-30 12:23:00 +0100

    minor staff

--- R/long2wide.R
+++ R/long2wide.R
@@ -1,9 +1,8 @@
 
 long2wide = function (data,type) {
-  require(V8)
   library(V8)
   js <- V8::v8(typed_arrays = TRUE)
-#js$source(file=system.file("js/underscore-min.js", package="V8"))
+  #js$source(file=system.file("js/underscore-min.js", package="V8"))
   js$source("https://raw.githubusercontent.com/esm-ispm-unibe-ch/dataformatter/master/js/underscore-min.js")
   js$source("https://raw.githubusercontent.com/esm-ispm-unibe-ch/dataformatter/master/js/combinations.js")
   js$source("https://raw.githubusercontent.com/esm-ispm-unibe-ch/dataformatter/master/js/long2wide.js")
--- test/test.R
+++ test/test.R
@@ -1,11 +1,14 @@
 Sys.setenv(LANG = "en")
-#rm(list=ls())
+rm(list=ls())
 library(readxl)
+source("../R/long2wide.R")
+source("../R/wide2long.R")
+
 wbin <- read_excel("./acutemania_netwide_multiarm_binary.xls")
 wcont <- read_excel("./CPPS_netwide_multiarm_continuous.xls")
 
-outbin = wide2Long(wbin,"binary")
+outbin = wide2long(wbin,"binary")
 print(outbin)
-outcont = wide2Long(wcont,"continuous")
+outcont = wide2long(wcont,"continuous")
 print(outcont)
 
